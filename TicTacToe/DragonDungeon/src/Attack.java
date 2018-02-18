5f6d2e227d40b14cbf65b1cd1d0bb42eca7ef51f
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2018-02-18 21:44:48 +0200

    Korjattu

--- TicTacToe/DragonDungeon/src/Attack.java
+++
@@ -1,5 +0,0 @@
-
-public class Attack {
-	String name;
-	int A_ID;
-}
--- TicTacToe/DragonDungeon/src/EffectInterface.java
+++
@@ -1,9 +0,0 @@
-public class EffectInterface {
-    public void Activate_effect(int A_ID) {}
-	public void Activate_effect(int A_ID, int target) {
-		
-	}
-	public void Activate_effect(int A_ID,int target,int target2) {
-		
-	}
-}
