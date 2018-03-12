9ec467b93e94cca351a4c47ba677c8d344e2fe95
janzu97 <36570867+janzu97@users.noreply.github.com>
2018-03-12 13:52:47 +0200

    Update Player.java

--- TicTacToe/DragonDungeon/src/Player.java
+++ TicTacToe/DragonDungeon/src/Player.java
@@ -14,6 +14,7 @@ public class Player implements Targetable{
 	boolean poison;
 	double armor=1.00;
 	String name="";
+	//Luo uuden pelaaja olion oletus Inventorylla
 	public Player() {
 		this.i=new Inventory();
 		i.addItem(new BagOfSalt());
@@ -22,32 +23,40 @@ public class Player implements Targetable{
 		i.addItem(new PotionOfInvigoration());
 		i.addItem(new PotionOfFortification());
 	}
+	//Luo parametrina saadusta listasta Inventoryn ja asettaa sen pelaajalle
 	public void SetInventory(String[] rigth){
 		this.i=new Inventory();
 		for(String s : rigth) {
 			i.addItem(ItemCreator.getItemByID(s));
 		}
 	}
+	//Palauttaa pelaajan Invetoryn
 	public String[] getInventory() {
 		return i.getInventory();
 	}
+	//Luo Uuden pelaaja olion jonka Inventory annetaan parametrina 
 	public Player(Inventory I) {
 		this.i=I;
 	}
+	// Lisää Loitsun loitsulistalle 
 	public void addSpell(Attack a){
 		Spells.add(a);
 	}
+	//Jäädyttää pelaajan
 	public void setFrozen(Freeze f) {
 		frozen=f;
 		
 	}
+	//Myrkyttää pelaajan
 	public void poisoned() {
 		poison=true;
 		SReader.read("You got poisoned!\n");
 	}
+	//Palautta armorin arvon
 	public double getArmor() {
 		return armor;
 	}
+	// asettaa armorin arvon
 	public void setArmor(double armor) {
 		this.armor=armor;
 	}
@@ -66,9 +75,11 @@ public class Player implements Targetable{
 		}
 
 	}
+	//Palauttaa pelaajan nimen
 	public String getname() {
 		return name;
 	}
+	//Vahingoittaa lohikäärmettä 
 	public void lightAttack(Targetable t) {
 		SReader.read("You launch into a quick attack.\n");
 		if(RNG.roll(80)) {
@@ -77,6 +88,7 @@ public class Player implements Targetable{
 			SReader.read("But you misssed.\n");
 		}
 	}
+	//Vahingoitaa lohikäärmettä enemmän
 	public void heavyAttack(Targetable t) {
 		SReader.read("You bring down your sword on the dragon with all your might\n");
 		if(RNG.roll(60)) {
@@ -86,9 +98,11 @@ public class Player implements Targetable{
 		}
 	}
 	@Override
+	//Palauttaa HP:n arvon
 	public int get_HP() {
 		return this.HP;
 	}
+	//Palauttaa pelaajan loitsut Arrayssa
 	public Attack[] getSpells() {
 		Attack[] a=new Attack[Spells.size()];
 		for(int i=0; i<Spells.size(); i++) {
@@ -97,6 +111,7 @@ public class Player implements Targetable{
 		return a;
 
 	}
+	//Palauttaa palauttaa loutsu valikon  
 	public String[] getSpellnamesForMenu() {
 		String[] a=new String[Spells.size()+1];
 		for(int i=0; i<Spells.size(); i++) {
@@ -107,10 +122,12 @@ public class Player implements Targetable{
 
 	}
 	@Override
+	//Palautta maksimi HP:n 
 	public int getmaxhp() {
 		return this.MAX_HP;
 	}
 	@Override
+	//Antaa vahinkoa fyysisestä hyökkäyksestä
 	public void takeDamage(int amount) {
 		if(poison) {
 			HP-=amount*armor*2;
@@ -126,9 +143,11 @@ public class Player implements Targetable{
 		}
 	}
 	@Override
+	//Antaa vahinkoa maagisesta hyökkäyksestä
 	public void takemagicDamage(int amount) {
 		HP=HP-amount;
 	}
+	//Nostaa armoria
 	public void ArmorUp(){
 		if(armor>0.125) {
 			armor=this.armor/2;
@@ -137,6 +156,7 @@ public class Player implements Targetable{
 			SReader.read("pelaaja's armor can't go any higher.\n");
 		}
 	}
+	//Piirtää HP palkin
 	public String getHpbar() {
 		String hp_bar="[";
 		double pros=(HP*10.0)/MAX_HP;
@@ -152,6 +172,7 @@ public class Player implements Targetable{
 		hp_bar+="]";
 		return hp_bar;
 	}
+	//Piirtää MP palkin
 	public String getMPbar() {
 		String MP_bar="[";
 		double pros=MP*10.0/Max_mp;
@@ -168,23 +189,28 @@ public class Player implements Targetable{
 		return MP_bar;
 	}
 	@Override
+	Vähentää manaa parametrin verran
 	public void useMana(int a) {
 		MP-=a;
 	}
 	@Override
+	//Palauttaa MP:n
 	public int getMp() {
 		return MP;
 	}
 	@Override
+	//Palauttaa maksimi MP:n
 	public int getmaxMp() {
 		return Max_mp;
 	}
 	@Override
+	//Asettaa parametrin MP:n arvoksi
 	public void setMP(int mp) {
 		this.MP=mp;
 		
 	}
 	@Override
+	//Alentaa armoria 
 	public void LowerArmor() {
 		if(armor<6) {
 			armor=armor*2;
