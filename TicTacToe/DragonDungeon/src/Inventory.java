00f593258c1e036450ca037dce0bf16bdde48e66
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2018-03-12 13:19:04 +0200

    Update Inventory.java

--- TicTacToe/DragonDungeon/src/Inventory.java
+++ TicTacToe/DragonDungeon/src/Inventory.java
@@ -10,13 +10,16 @@ public class Inventory {
 	}
 	public void addItem(Item i) {
 		items.add(i);
+		//lisää itemin inventoriin
 	}
 	public Item getItem(int i) {
 		return items.get(i);
+		//palauttaa itemin annetusta indeksista
 	}
 	
 	
 	public void activate_Item(Item i) {
+		//aktivoi itemin effektin ja poistaa sen inventorista
 		if(items.contains(i)) {
 			for (Item item : items) {
 				if(item.equals(i)) {
@@ -28,6 +31,7 @@ public class Inventory {
 		}
 	}
 	public String[] getInventory() {
+		//palauttaa inventorin sisallon listana String olioita
 		String[] List=new String[items.size()];
 		for(int i=0; i<items.size(); i++) {
 			List[i]=items.get(i).toString();
@@ -35,6 +39,7 @@ public class Inventory {
 		return List;
 	}
 	public String[] getInventoryforMenu() {
+		//palauttaa inventorin sisallon listana String olioita menuun sopivaksi muotoiltuna
 		String[] List=new String[items.size()+1];
 		for(int i=0; i<items.size(); i++) {
 			List[i]=items.get(i).toString().replaceAll("_", " ");;
