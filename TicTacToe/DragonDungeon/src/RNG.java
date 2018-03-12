495cf2c52e726bb166ad94725eb107d4e0c7fc05
BeadsInCheese <36571102+BeadsInCheese@users.noreply.github.com>
2018-03-12 13:45:38 +0200

    Update RNG.java

--- TicTacToe/DragonDungeon/src/RNG.java
+++ TicTacToe/DragonDungeon/src/RNG.java
@@ -3,10 +3,11 @@ import java.util.Random;
 public class RNG {
 	static Random r=new Random();
 	public static int rint(int range) {
-		
+		//palauttaa numeron valilta 0-parametri
 		return r.nextInt(range);
 	}
 	public static boolean roll(int chance) {
+		//Tekee testin joka onnistuu tietylla todennakoisyydella
 		if(r.nextInt(100)<100-chance) {
 			return true;
 		}else {
